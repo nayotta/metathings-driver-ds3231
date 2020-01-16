0e55a4caf7afcf2c01f874cecaeff3d86f7ade0f
zhanghuichina <zhanghui0000@gmail.com>
2020-01-16 23:01:53 +0800

    :bug: fix cache memory leak

--- components/modbus_airswitch001/cache_airswitch001.h
+++ components/modbus_airswitch001/cache_airswitch001.h
@@ -1,30 +1,31 @@
 #ifndef _CAHCE_AIRSWITCH001_H
 #define _CAHCE_AIRSWITCH001_H
 
-#include "stdio.h"
-#include "stdbool.h"
 #include "esp_err.h"
 #include "mt_port_airswitch001.h"
+#include "stdbool.h"
+#include "stdio.h"
 
-struct cache_t
-{
-    int32_t num_master;
-    int32_t *masters;
-    int32_t num_slaver;
-    int32_t *slavers;
+struct cache_t {
+  int32_t num_master;
+  int32_t *masters;
+  int32_t num_slaver;
+  int32_t *slavers;
 };
 
 typedef struct cache_t Cache_t;
 
-esp_err_t cache_get(Cache_t *cache);
+Cache_t *cache_get();
 
 esp_err_t cache_set(Cache_t *cache);
+
+void cache_free(Cache_t *cache);
 
 bool cahce_diff(Cache_t *cache1, Cache_t *cache2);
 
 void cache_from_modbus(UCHAR slaveAddr, Cache_t *cache);
 
-esp_err_t cache_get_and_check(Cache_t *cache, int num_master, int num_slaver);
+Cache_t *cache_get_and_check(int num_master, int num_slaver);
 
 int *cache_convert(Cache_t *cache, int *num);
 
@@ -32,9 +33,8 @@ esp_err_t cache_get_target(int addr_in, UCHAR *target);
 
 esp_err_t cache_task();
 
-esp_err_t cache_quality_get(int addr, int32_t *quality,
-                            int32_t *quality_save);
-                        
+esp_err_t cache_quality_get(int addr, int32_t *quality, int32_t *quality_save);
+
 esp_err_t cache_quality_set(int addr, int32_t quality);
 
 #endif
