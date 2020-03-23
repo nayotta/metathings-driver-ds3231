6a1608b674a404eb13ae9fc36bc0a98ca302ddbf
zhanghuichina <zhanghui0000@gmail.com>
2020-03-23 16:23:04 +0800

    :tada: modbus driver and example for power001

---
+++ mt_apps/modbus_power001/modbus_power001.h
@@ -0,0 +1,48 @@
+#ifndef _MODBUS_POWER001_H
+#define _MODBUS_POWER001_H
+
+#include "esp_err.h"
+#include "esp_log.h"
+
+// define =====================================================================
+
+typedef struct _modbus_power001_data_t {
+  double quality;
+  double voltage;
+  double current;
+} modbus_power001_data_t;
+
+typedef struct _modbus_power001_datas_t {
+  int port_num;
+  modbus_power001_data_t **datas;
+} modbus_power001_datas_t;
+
+// help func ==================================================================
+
+modbus_power001_data_t *modbus_power001_new_data();
+
+void modbus_power001_free_data(modbus_power001_data_t *data);
+
+modbus_power001_datas_t *modbus_power001_new_datas(int size);
+
+void modbus_power001_free_datas(modbus_power001_datas_t *datas);
+
+// func =======================================================================
+
+esp_err_t modbus_power001_init(int tx_pin, int rx_pin, int en_pin);
+
+// single api
+
+esp_err_t modbus_power001_get_quality(uint8_t port, double *data);
+
+esp_err_t modbus_power001_get_voltage(uint8_t port, double *data);
+
+esp_err_t modbus_power001_get_current(uint8_t port, double *data);
+
+// complex api
+
+modbus_power001_data_t *modbus_power001_get_data(int port);
+
+modbus_power001_datas_t *modbus_power001_get_datas();
+
+#endif
