e9e8a8074c3f0e2d1ca7a57cc246c338474fc460
zhanghuichina <zhanghui0000@gmail.com>
2020-05-27 19:36:51 +0800

    :art: add maglock flow and mqtt

---
+++ mt_apps/gpio_maglock001/gpio_maglock001_module_flow.h
@@ -0,0 +1,9 @@
+#ifndef _GPIO_MAGLOCK001_MODULE_FLOW_H
+#define _GPIO_MAGLOCK001_MODULE_FLOW_H
+
+#include "mt_module_flow.h"
+#include "string.h"
+
+void gpio_maglock001_module_flow_task(mt_module_flow_t *module_flow);
+
+#endif
