99aac6661268927ec0ba71df803905f14f956ac8
zhanghuichina <zhanghui0000@gmail.com>
2020-07-11 23:22:50 +0800

    :art: update bits001

--- mt_apps/modbus_bits001/modbus_bits001.h
+++ mt_apps/modbus_bits001/modbus_bits001.h
@@ -6,6 +6,8 @@
 
 #include "esp_err.h"
 
+// define =====================================================================
+
 #ifndef CONFIG_LOG_DEFAULT_LEVEL
 #define CONFIG_LOG_DEFAULT_LEVEL ESP_LOG_DEBUG
 #endif
@@ -14,28 +16,17 @@
 #define CONFIG_FREERTOS_HZ 100
 #endif
 
-#define BUF_MAXLEN 64
+// func =======================================================================
 
-struct RetMsg_t // ret message struct
-{
-  UCHAR retBuf[BUF_MAXLEN]; // ret buffer
-  USHORT recvCmd;           // ret addr
-  USHORT retLen;            // ret len
-} * RetMsg;                 // global save point
+esp_err_t modbus_bits001_init(uint8_t port, int tx_pin, int rx_pin, int en_pin);
 
-eMBErrorCode modbus_bits001_init(UCHAR ucPort, ULONG ulBaudRate,
-                                 eMBParity eParity, int tx_pin, int rx_pin,
-                                 int en_pin);
+esp_err_t modbus_bits001_get_temp(int addr, double *temp);
 
-esp_err_t mt_bits001_get_temp(int addr, double *temp);
+esp_err_t modbus_bits001_get_hum(int addr, double *hum);
 
-esp_err_t mt_bits001_get_hum(int addr, double *hum);
+esp_err_t modbus_bits001_get_frog(int addr, double *frog);
 
-esp_err_t mt_bits001_get_frog(int addr, double *frog);
-
-esp_err_t mt_bits001_get_data(int addr, double *temp, double *hum,
-                              double *frog);
-
-void mt_modbus_bits001_task();
+esp_err_t modbus_bits001_get_data(int addr, double *temp, double *hum,
+                                  double *frog);
 
 #endif
